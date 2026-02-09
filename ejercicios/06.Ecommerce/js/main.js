9a5ec17d57b03bb7dd880c4bf5871f76f599edb6
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2026-02-09 11:37:42 -0500

    :sparkles: se agrega los productos desde productos.js

---
+++ ejercicios/06.Ecommerce/js/main.js
@@ -0,0 +1,19 @@
+const contenedorProductos = document.querySelector(".contenedor-productos");
+
+contenedorProductos.innerHTML = ""; // por seguridad
+
+productos.forEach(producto => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+
+    div.innerHTML = `
+        <img class="producto-imagen" src="${producto.imagen}" alt="">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.seccion}</h3>
+            <p class="producto-precio">$${producto.precio}</p>
+            <button class="producto-agregar">Agregar</button>
+        </div>
+    `;
+
+    contenedorProductos.appendChild(div);
+});
--- ejercicios/06.Ecommerce/js/productos.js
+++ ejercicios/06.Ecommerce/js/productos.js
@@ -18,5 +18,5 @@ const productos = [
         seccion: "Abrigo",
         imagen: "https://ae-pic-a1.aliexpress-media.com/kf/Sc3471cbb48a2472d8c5601e21a9d7933t.jpg?has_lang=1&ver=2_960x960q75.jpg_.avif",
         precio: 2000
-    },
+    }
 ]
