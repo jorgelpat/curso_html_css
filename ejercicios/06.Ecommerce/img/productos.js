4515aaeaaabcf79b3de5618a9fdb6c2a35428636
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2026-02-08 19:43:18 -0500

    :memo: se crea productos.js y se sube datos de imagenes

---
+++ ejercicios/06.Ecommerce/img/productos.js
@@ -0,0 +1,17 @@
+const productos = [
+    {
+        seccion: "Abrigo",
+        imagen: "https://down-co.img.susercontent.com/file/c3f52e78d0a7917d56a99368a4c35d5a_tn.webp",
+        precio: 1000
+    },
+    {
+        seccion: "Abrigo",
+        imagen: "https://ae-pic-a1.aliexpress-media.com/kf/S8bb0e96855904ace8136f8eb95a55e8fM.jpg_640x640q75.jpg_.avif",
+        precio: 2000
+    },
+    {
+        seccion: "Abrigo",
+        imagen: "https://ae-pic-a1.aliexpress-media.com/kf/Saf7d91d524a54cc09b1b4fc8349e0c44D.jpg_960x960q75.jpg_.avif",
+        precio: 2000
+    }
+]
